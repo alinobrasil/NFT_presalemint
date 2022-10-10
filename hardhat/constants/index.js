bfe073067b365d92702841aa599c54acdc3fb3ff
Ali Kim <alikim@gmail.com>
2022-10-10 01:42:07 -0400

    new contract

--- hardhat/constants/index.js
+++ hardhat/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x074BFee2f332236a5f9E5A8Ae467CE3DB4De2FA0";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nftpresale-nhzgc0igo-alinobrasil.vercel.app/api/";
+const METADATA_URL = "https://nftpresale1.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
---
+++ hardhat/scripts/arguments.js
@@ -0,0 +1,4 @@
+module.exports = [
+    "0x074BFee2f332236a5f9E5A8Ae467CE3DB4De2FA0",
+    "https://nftpresale1.vercel.app/api/"
+]
